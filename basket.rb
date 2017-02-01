c00f184f186005a0a454774e0292a509fe6eadb2
Hrishi Mittal <hrishimittal@gmail.com>
2017-02-01 12:09:50 +0000

    Fix indentation

--- basket.rb
+++ basket.rb
@@ -1,23 +1,23 @@
 class Basket
-	# All prices are in pence/cents for avoiding rounding errors.
-	CATALOG = [
-		{
-			name:"Jeans",
-			code: :J01,
-			price: 3295,
-			promotion: ->(units){units - (units/2)/2.0 }
-		},
-		{
-			name:"Blouse",
-			code: :B01,
-			price: 2495
-		},
-		{
-			name:"Socks",
-			code: :S01,
-			price: 795
-		}
-	]
+  # All prices are in pence/cents for avoiding rounding errors.
+  CATALOG = [
+    {
+      name:"Jeans",
+      code: :J01,
+      price: 3295,
+      promotion: ->(units){units - (units/2)/2.0 }
+    },
+    {
+      name:"Blouse",
+      code: :B01,
+      price: 2495
+    },
+    {
+      name:"Socks",
+      code: :S01,
+      price: 795
+    }
+  ]
 
   def initialize(items = {J01:0, B01:0, S01:0}, total = 0)
     @items = items
@@ -25,47 +25,47 @@ class Basket
   end
 
   def add(product_code)
-  	@items[product_code.to_sym] += 1
+    @items[product_code.to_sym] += 1
   end
 
   def total
-  	items_total = calculate_items_total
-  	convert_to_amount(items_total + calculate_delivery_charges(items_total))
+    items_total = calculate_items_total
+    convert_to_amount(items_total + calculate_delivery_charges(items_total))
   end
 
   private
-  	def calculate_items_total
-  		CATALOG.map{|p| product_total(p[:code], @items[p[:code]]) }.inject(:+)
-  	end
+    def calculate_items_total
+      CATALOG.map{|p| product_total(p[:code], @items[p[:code]]) }.inject(:+)
+    end
 
-  	def product_total(product_code, units)
-  		price = find_product_in_catalog(product_code)[:price]
-  		price * apply_promotion(product_code, units)
-  	end
+    def product_total(product_code, units)
+      price = find_product_in_catalog(product_code)[:price]
+      price * apply_promotion(product_code, units)
+    end
 
-  	def apply_promotion(product_code, units)
-  		if promotion = find_product_in_catalog(product_code)[:promotion]
-  			promotion.call(units)
-  		else
-  			units
-  		end
-  	end
+    def apply_promotion(product_code, units)
+      if promotion = find_product_in_catalog(product_code)[:promotion]
+        promotion.call(units)
+      else
+        units
+      end
+    end
 
-  	def find_product_in_catalog(product_code)
-  		CATALOG.select{|p| p[:code] == product_code }[0]
-  	end
+    def find_product_in_catalog(product_code)
+      CATALOG.select{|p| p[:code] == product_code }[0]
+    end
 
-	  def calculate_delivery_charges(basket_total)
-	  	if basket_total < 5000
-	  		495
-	  	elsif basket_total < 9000
-	  		295
-	  	else
-	  		0
-	  	end
-	  end
+    def calculate_delivery_charges(basket_total)
+      if basket_total < 5000
+        495
+      elsif basket_total < 9000
+        295
+      else
+        0
+      end
+    end
 
-	  def convert_to_amount(cents)
-	  	cents.floor/100.0
-	  end
+    def convert_to_amount(cents)
+      cents.floor/100.0
+    end
 end
--- basket_spec.rb
+++ basket_spec.rb
@@ -1,24 +1,24 @@
 require './basket.rb'
 
 RSpec.describe Basket do
-	let(:basket) { Basket.new }
-	it "should calculate correct basket totals - S01, B01" do
-		["S01", "B01"].each {|p|  basket.add(p) }
-		expect(basket.total).to eq(37.85)
-	end
+  let(:basket) { Basket.new }
+  it "should calculate correct basket totals - S01, B01" do
+    ["S01", "B01"].each {|p|  basket.add(p) }
+    expect(basket.total).to eq(37.85)
+  end
 
-	it "should calculate correct basket totals - J01, J01" do
-		["J01", "J01"].each {|p|  basket.add(p) }
-		expect(basket.total).to eq(54.37)
-	end
+  it "should calculate correct basket totals - J01, J01" do
+    ["J01", "J01"].each {|p|  basket.add(p) }
+    expect(basket.total).to eq(54.37)
+  end
 
-	it "should calculate correct basket totals - J01, B01" do
-		["J01", "B01"].each {|p|  basket.add(p) }
-		expect(basket.total).to eq(60.85)
-	end
+  it "should calculate correct basket totals - J01, B01" do
+    ["J01", "B01"].each {|p|  basket.add(p) }
+    expect(basket.total).to eq(60.85)
+  end
 
-	it "should calculate correct basket totals - S01, S01, J01, J01, J01" do
-		["S01", "S01", "J01", "J01", "J01"].each {|p|  basket.add(p) }
-		expect(basket.total).to eq(98.27)
-	end
+  it "should calculate correct basket totals - S01, S01, J01, J01, J01" do
+    ["S01", "S01", "J01", "J01", "J01"].each {|p|  basket.add(p) }
+    expect(basket.total).to eq(98.27)
+  end
 end
